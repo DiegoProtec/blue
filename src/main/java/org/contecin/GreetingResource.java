de40962910d14cf1230a79163fc711d5ebf1edce
Diego Fernandes <diego.fernandes@contecin.com>
2024-01-02 01:35:40 -0300

    primeira pagina

--- src/main/java/org/contecin/GreetingResource.java
+++ src/main/java/org/contecin/GreetingResource.java
@@ -5,12 +5,16 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@Path("/blue")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces({MediaType.TEXT_HTML})
+    public InputStream hello() {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        return classloader.getResourceAsStream("blue.html");
     }
 }
